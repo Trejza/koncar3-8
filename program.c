e641d1401bd0456b961f5f0872c077136049f5f8
Trejza <nemanja.putnikovic0@gmail.com>
2025-12-05 09:27:40 +0100

    nesto sam promenio

--- program.c
+++ program.c
@@ -3,5 +3,22 @@ int main()
 {
   printf("dobro dosli na kurs Linux-a\n");
   printf("Dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  printf("dobro dosli na kurs Linux-a\n");
+  
   return 0;
 }
